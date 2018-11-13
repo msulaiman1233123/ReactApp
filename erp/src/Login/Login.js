5bf0869f427c75dd6d5b3cdb3413fae3ed4636b4
Muhammad Sulaiman <muhammad.sulaiman@avanza.pk>
2018-11-13 14:08:34 +0500

    login module

---
+++ erp/src/Login/Login.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class Login extends React.Component{
+	
+	constructor(){
+		super();
+	}
+	
+	render(){
+		return(
+			<div>
+				<div className='container text-center'>Welcome to React JS</div>
+				
+			</div>
+		)
+	}
+	
+}
+export default Login;
---
+++ erp/src/Main.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import Login from './Login/Login'
+
+class Main extends React.Component{
+	
+	render(){
+		return(
+				<Login />
+			)
+	}
+	
+}
+export default Main;
