354f836c36f0f6466439723f74306d662f82140b
Aftab Ali <aftab.ali@avanza.pk>
2018-11-15 14:38:15 +0500

    Merged Main.js

--- erp/src/Main.js
+++ erp/src/Main.js
@@ -16,7 +16,7 @@ class Main extends React.Component{
 					  <button type="button" onClick={()=> this.openComponent('Login')} class="btn btn-primary btn-sm">Login</button>
 					  <button type="button" onClick={()=> this.openComponent('Register')} class="btn btn-secondary btn-sm">Register</button>
 					 { this.state.component=='Login' ? <Login/> : null }
-					 {this.state.component=='Register' ? '</Registration>: null}
+					 {this.state.component=='Register' ? <Registration/>: null}
 				</div>
 			);
 
