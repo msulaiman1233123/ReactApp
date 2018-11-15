8d53244c5f783ada1bd45f2d83350f8a4e8a1739
Muhammad Sulaiman <muhammad.sulaiman@avanza.pk>
2018-11-15 14:35:46 +0500

    test

--- erp/src/Main.js
+++ erp/src/Main.js
@@ -11,24 +11,15 @@ class Main extends React.Component{
 	
 	render(){
 		return(
-<<<<<<< HEAD
 
-=======
->>>>>>> 0851e3e8dc5da02fcc87e3bea0d3bdd656de7801
 				<div >
 					  <button type="button" onClick={()=> this.openComponent('Login')} class="btn btn-primary btn-sm">Login</button>
 					  <button type="button" onClick={()=> this.openComponent('Register')} class="btn btn-secondary btn-sm">Register</button>
 					 { this.state.component=='Login' ? <Login/> : null }
-<<<<<<< HEAD
 					 {this.state.component=='Register' ? '</Registration>: null}
 				</div>
-
-			)
-=======
-					 {this.state.component=='Register' ? <Registration/>: null}
-				</div>
 			);
->>>>>>> 0851e3e8dc5da02fcc87e3bea0d3bdd656de7801
+
 	}
 	openComponent(component){
 		this.setState({component: component});
