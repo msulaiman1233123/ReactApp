5e0e6d5fea404457e8b73bea5cc86e5cfcc0c835
Muhammad Sulaiman <muhammad.sulaiman@avanza.pk>
2018-11-15 14:08:15 +0500

    COnfilict resolution

--- erp/src/Main.js
+++ erp/src/Main.js
@@ -3,10 +3,23 @@ import Login from './Login/Login'
 
 class Main extends React.Component{
 	
+	constructor(){
+		super();
+		this.state = {component: ''};
+	}
+	
 	render(){
 		return(
-				<Login />
+				<div >
+					  <button type="button" onClick={()=> this.openComponent('Login')} class="btn btn-primary btn-sm">Login</button>
+					  <button type="button" onClick={()=> this.openComponent('Register')} class="btn btn-secondary btn-sm">Register</button>
+					 { this.state.component=='Login' ? <Login/> : null }
+					 {this.state.component=='Register' ? 'Register': null}
+				</div>
 			)
+	}
+	openComponent(component){
+		this.setState({component: component});
 	}
 	
 }
